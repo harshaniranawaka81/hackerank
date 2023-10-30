10e55fc6df2af624a364a31eb3d321ce1d324245
Harshani Ranawaka <99527713+harshaniranawaka81@users.noreply.github.com>
2023-10-30 17:24:35 +0000

    Add more challenges

---
+++ FlippingBits/FlippingBits/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+long n = 4;
+
+var bitsForNum  = new StringBuilder();
+var bitsForFlippedNum = new StringBuilder();
+
+var bitsForNumValues = new List<int>();
+
+var remainder = n;
+
+while(remainder > 0)
+{
+    bitsForNumValues.Add((int)(remainder % 2));
+    remainder /= 2;
+}
+
+for (int i = bitsForNumValues.Count - 1; i >= 0; i--)
+{
+    bitsForNum.Append(bitsForNumValues[i]);
+}
+
+var formattedBinaryNum = bitsForNum.ToString().PadLeft(32, '0');
+
+Console.WriteLine(formattedBinaryNum);
+
+for (int i = 0; i < formattedBinaryNum.Length; i++)
+{
+    if (formattedBinaryNum[i] == '0')
+    {
+        bitsForFlippedNum.Append(1);
+    }
+    else if (formattedBinaryNum[i] == '1')
+    {
+        bitsForFlippedNum.Append(0);
+    }
+}
+
+Console.WriteLine(bitsForFlippedNum.ToString());
+
+var sum = 0l;
+
+for (int i = bitsForFlippedNum.Length - 1; i >= 0; i--)
+{
+    var value = int.Parse(bitsForFlippedNum.ToString(i,1));
+    var index = bitsForFlippedNum.Length - i - 1;
+    var power = (long)Math.Pow(2, index);
+    sum += value * power;
+}
+
+Console.WriteLine(sum);
---
+++ TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string s = "07:45:00PM";
+
+string amPmPart = s.Substring(s.Length - 2);
+int hour = int.Parse(s.Substring(0, 2));
+int minute = int.Parse(s.Substring(3, 2));
+int second = int.Parse(s.Substring(6, 2));
+
+if (amPmPart == "PM")
+{
+    if (hour > 12)
+        hour -= 12;
+    else if (hour < 12)
+        hour += 12;
+}
+else if (amPmPart == "AM")
+{
+    if (hour == 12)
+        hour -= 12;    
+}
+
+string convertedTime = $"{hour.ToString("00")}:{minute.ToString("00")}:{second.ToString("00")}";
+
+Console.WriteLine(convertedTime);
+
